cf0870af6323fb7c4e01cdbd3487b4780d3385fe
labaking10786 <s90107024@gmail.com>
2022-07-12 16:11:31 +0800

    完成登入功能

--- src/main.js
+++ src/main.js
@@ -1,6 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import '@/assets/css/global.css'
+
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
+
+import 'element-ui/lib/theme-chalk/index.css'
+
+import axios from 'axios'
+axios.defaults.baseURL = 'http://192.168.103.119:8888/api/private/v1/'
+
+Vue.prototype.$http = axios
+Vue.prototype.$message = Message
+
+Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
 
 Vue.config.productionTip = false
 
