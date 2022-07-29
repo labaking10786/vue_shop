7546c1f56f591fc8c99d08eb8ce69a9458b9e320
labaking10786 <s90107024@gmail.com>
2022-07-29 11:16:52 +0800

    完成報表

---
+++ src/components/report/Report.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首頁</el-breadcrumb-item>
+      <el-breadcrumb-item>數據統計</el-breadcrumb-item>
+      <el-breadcrumb-item>數據報表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-card>
+      <div id="main" style="width: 600px;height:400px;"></div>
+    </el-card>
+  </div>
+</template>
+
+<script>
+import * as echarts from 'echarts'
+import _ from 'lodash'
+
+export default {
+  name: 'Report',
+  data () {
+    return {
+      options: {
+        title: {
+          text: '用户来源'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross',
+            label: {
+              backgroundColor: '#E9EEF3'
+            }
+          }
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: [
+          {
+            boundaryGap: false
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ]
+      }
+    }
+  },
+  created () {
+
+  },
+  async mounted () {
+    let myChart = echarts.init(document.getElementById('main'))
+
+    const { data: res } = await this.$http.get('reports/type/1')
+    if (res.meta.status !== 200) return this.$message.error('取圖表數據失敗')
+    console.log(res.data)
+    const result = _.merge(res.data, this.options)
+    // 使用刚指定的配置项和数据显示图表。
+    myChart.setOption(result)
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
